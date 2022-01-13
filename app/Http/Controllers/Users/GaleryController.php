e716147564aa7253250ed19d26b2752c3c1b93c9
Naufal Farros <naufalfarros05@gmail.com>
2022-01-13 15:14:11 +0700

    controller galery

--- app/Http/Controllers/Users/GaleryController.php
+++ app/Http/Controllers/Users/GaleryController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\photoGalery;
 use Illuminate\Http\Request;
 
 class GaleryController extends Controller
 {
-    //
+    
+    public function index(){
+        $photo = photoGalery::all();
+        return view('User.Galery.galery',compact('photo'));
+    }
 }
