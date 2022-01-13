16e22f6a56d90302a4318f83b8c58f3dde7beb6e
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2022-01-13 16:46:05 +0700

    Update donate

--- resources/views/User/About Us/donate.blade.php
+++ resources/views/User/About Us/donate.blade.php
@@ -1,54 +1,58 @@
 @extends('User.web')
 
 @section('content')
-<div class="donation-wrapper shadow">
+    <div class="donation-wrapper shadow">
 
         <section class="donation-subcribe text-center">
-              <div class="donation">
-                     <h1>Mari Beramal Jariyah Untuk Masjid <br> Abu Bakar As Shiddiq</h1>
-                     <p>"Jika untuk memiliki rumah di dunia saja harus berusaha keras, apalagi untuk mendapat rumah di Surga "</p>
-                     <h3>"Siapa yang membangun masjid karena Allah, maka Allah akan membangun baginya semisal itu di surga." <br>
-                        (HR. Bukhari No. 450 dan Muslim No. 533)
-                    </h3>
-                     
-                 </div>
-             </div>
-        </section>
+            <div class="donation">
+                <h1>Mari Beramal Jariyah Untuk Masjid <br> Abu Bakar As Shiddiq</h1>
+                <p>"Jika untuk memiliki rumah di dunia saja harus berusaha keras, apalagi untuk mendapat rumah di Surga "
+                </p>
+                <h3>"Siapa yang membangun masjid karena Allah, maka Allah akan membangun baginya semisal itu di surga." <br>
+                    (HR. Bukhari No. 450 dan Muslim No. 533)
+                </h3>
 
-    <div class="container ">
-        <div class="container shadow card-donate-masjid">
-            
-            <h3 class="text-center">Masjid Abu Bakar As-Shiddiq</h3>
-
-                    <h4>Transfer</h4>              
-                    <h3>No Rekening : 120312301283912</h3>
-                    <h3>Atas Nama : Masjid Abu Bakar As-Shiddiq</h3>
-                   
-                    <div class="mt-5">
-            <h4>Bendahara 1</h4>
-            
-            <h3>Nama : Bpk. Saiful</h3>
-            <h3>Alamat : Jln. Kaca Piring</h3>
-            <h3>No : 081231231</h3>
-        </div>
-            
-
-            <div class="mt-5">
-                <h4>Bendahara 2</h4>
-            
-            <h3>Nama : Naufal Farros </h3>
-            <h3>Alamat : Jln. Kaca Piring</h3>
-            <h3>No : 081231231</h3>
             </div>
-                
-        </div>
-    
-
-        
-        
-        
     </div>
+    </section>
+
+    <div class="container donation-page">
+        <div class="container shadow card-donate-masjid">
+
+            <h2 class="text-center">Masjid Abu Bakar As-Shiddiq</h2>
+
+            <div class="atm text-center">
+                <h4>Transfer</h4>
+                <h3 class="text-center">No Rekening : 120312301283912 (Masjid Abu Bakar As-Shiddiq)</h3>
+            </div>
+
+            <div class="row mt-4">
+                <div class="col">
+                    <h4 class="text-center">Bendahara 1</h4>
+    
+                    <h3 class="text-center">Bpk. Saiful</h3>
+                    <h3 class="text-center">Jln. Kaca Piring</h3>
+                    <h3 class="text-center">No Hp : 081231231</h3>
+                </div>
     
     
-               
+                <div class="col">
+                    <h4 class="text-center">Bendahara 2</h4>
+    
+                    <h3 class="text-center">Naufal Farros </h3>
+                    <h3 class="text-center">Jln. Kaca Piring</h3>
+                    <h3 class="text-center">No Hp : 081231231</h3>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+    </div>
+
+
+
 @endsection
